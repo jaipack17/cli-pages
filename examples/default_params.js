c2ffc238a6a75ae4e82c2329fb7358212f9f92c4
jaipack17 <74130881+jaipack17@users.noreply.github.com>
2021-07-13 10:18:20 +0530

    Update default_params.js

--- examples/default_params.js
+++ examples/default_params.js
@@ -18,7 +18,7 @@ pages = [
 
 const pages = require("cli-pages");
 
-let book = new pages.Book([
+let _book = new pages.Book([
     {
         title: "Amazing!",
         content: "hello! this is an example that shows you the parameters when using the Book class!",
