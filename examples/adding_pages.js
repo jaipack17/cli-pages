c850f5a0b282bf13730a105d153b8541d6359a90
jaipack17 <74130881+jaipack17@users.noreply.github.com>
2021-07-13 10:17:32 +0530

    Update adding_pages.js

--- examples/adding_pages.js
+++ examples/adding_pages.js
@@ -1,6 +1,6 @@
 const pages = require("cli-pages");
 
-let book = new pages.Book([
+let _book = new pages.Book([
     {
         title: "Amazing!",
         content: "hello! this is an example that shows you the parameters when using the Book class!",
